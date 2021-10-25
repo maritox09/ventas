14b9ff48b9ebb325b427fcd8901ca4be202ed120
maritox09 <41440214+maritox09@users.noreply.github.com>
2021-10-24 18:58:24 -0600

    Update Conexion.java

--- src/main/java/Config/Conexion.java
+++ src/main/java/Config/Conexion.java
@@ -17,7 +17,7 @@ public class Conexion {
 
         try {
               Class.forName("com.mysql.jdbc.Driver");
-              con=DriverManager.getConnection("jdbc:mysql://192.168.5.193:3306/Ventas","root","example");
+              con=DriverManager.getConnection("jdbc:mysql://localhost:3306/Ventas?","root","example");
 
         } catch (Exception e) {
             
